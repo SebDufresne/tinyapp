724f6572b8a653b197d9a649f4932dda6cd782a1
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-08 22:49:52 +0000

    Created longURL in shortURL redirect

--- express_server.js
+++ express_server.js
@@ -29,7 +29,6 @@ const createUniqueKey = (keyObject) => {
   return aKey;
 };
 
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
@@ -44,6 +43,12 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log(longURL);
+  res.redirect(longURL);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
