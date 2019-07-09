54388ad832f95fa64a0f21ef61d5783a2ca237b0
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-09 18:54:20 +0000

    Added Edit button to urls_index

--- express_server.js
+++ express_server.js
@@ -56,6 +56,12 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls/:shortURL/update", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
