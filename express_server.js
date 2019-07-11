52c41b7643d6db55d3433e5b3872e4b6fdc34c10
Sebastien Dufresne <s.dufresne@dreaminggiraffe.ca>
2019-07-11 05:20:00 +0000

    Standardized delimiter and string identifier

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
-const express = require("express");
+const express = require('express');
 const cookieSession = require('cookie-session');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const getUserByEmail = require('./helpers').getUserByEmail;
 
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8080; // default port 8080
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 const saltRounds = 10;
 app.use(cookieSession({
@@ -45,43 +45,44 @@ const urlsForUser = id => {
 
 
 const urlDatabase = {
-  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID" },
-  "b6UTxQ": { longURL: "https://www.tsn.ca", userID: "userRandomID" },
-  "9sm5xK": { longURL: "http://www.google.com", userID: "user2RandomID" },
-  "i3BoGr": { longURL: "https://www.google.ca", userID: "user2RandomID" }
+  'b2xVn2': { longURL: 'http://www.lighthouselabs.ca', userID: 'userRandomID' },
+  'b6UTxQ': { longURL: 'https://www.tsn.ca', userID: 'userRandomID' },
+  '9sm5xK': { longURL: 'http://www.google.com', userID: 'user2RandomID' },
+  'i3BoGr': { longURL: 'https://www.google.ca', userID: 'user2RandomID' }
 };
 
 const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "$2b$10$STqQSDAFepyPnfKpL.QYfOu2p7kk3wZ0s5lFSoHQQp91nJjMymYXG"
+  'userRandomID': {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: '$2b$10$STqQSDAFepyPnfKpL.QYfOu2p7kk3wZ0s5lFSoHQQp91nJjMymYXG'
   },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "$2b$10$hF18oxdHmO/ToDB6S.QvD.5Gm0duoml5x0C1bkYKDnuiAQmFhOxqC"
+  'user2RandomID': {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: '$2b$10$hF18oxdHmO/ToDB6S.QvD.5Gm0duoml5x0C1bkYKDnuiAQmFhOxqC'
   }
 };
 
 // Retrieve login page OR urls
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   const user = users[req.session.userId] || '';
   if (user) {
-    res.redirect("/urls");
+    res.redirect('/urls');
   } else {
-    res.redirect("/login");
+    res.redirect('/login');
   }
 });
 
 // Retrieve (GET) register page
-app.get("/register", (req, res) => {
+app.get('/register', (req, res) => {
   const user = users[req.session.userId] || '';
-  res.render('register', {user, statusCode: 200});
+  const templateVars = {user, statusCode: 200};
+  res.render('register', templateVars);
 });
 
 // Submit (POST) register page
-app.post("/register", (req, res) => {
+app.post('/register', (req, res) => {
   const user = users[req.session.userId] || '';
   if (!req.body.email || !req.body.password) {
     res.status(400);
@@ -91,68 +92,71 @@ app.post("/register", (req, res) => {
     const uniqID = createUniqueKey(urlDatabase);
     users[uniqID] = {id : uniqID, email: req.body.email, password: bcrypt.hashSync(req.body.password, saltRounds) };
     res.cookie('user_id',uniqID);
-    res.redirect("/urls");
+    res.redirect('/urls');
   }
 
   if (res.statusCode === 400) {
-    res.render('register', {user, statusCode: 400});
+    const templateVars = {user, statusCode: 2400};
+    res.render('register', templateVars);
   }
 });
 
 // Retrieve (GET) login page
-app.get("/login", (req, res) => {
+app.get('/login', (req, res) => {
   const user = users[req.session.userId] || '';
-  res.render('login', {user, statusCode: 200});
+  const templateVars = {user, statusCode: 200};
+  res.render('login', templateVars);
 });
 
 // Submit (POST) login page
-app.post("/login", (req, res) => {
+app.post('/login', (req, res) => {
   const user = users[req.session.userId] || '';
   const idFromEmail = getUserByEmail(req.body.email,users);
   if (!idFromEmail) {
     res.status(403);
   } else if (bcrypt.compareSync(req.body.password,users[idFromEmail].password)) {
     req.session.userId = idFromEmail;
-    res.redirect("/urls");
+    res.redirect('/urls');
   } else {
     res.status(403);
   }
   if (res.statusCode === 403) {
-    res.render('login', {user, statusCode: 403});
+    const templateVars = {user, statusCode: 403};
+    res.render('login', templateVars);
   }
 });
 
 // Logout functionnality (removes cookie)
-app.post("/logout", (req, res) => {
+app.post('/logout', (req, res) => {
   req.session = null;
-  res.redirect("/urls");
+  res.redirect('/urls');
 });
 
 // Redirect to another page through a (GET) request
-app.get("/u/:shortURL", (req, res) => {
+app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
 
 // Retrieve (GET) and display a list of URLs for a user
-app.get("/urls", (req, res) => {
+app.get('/urls', (req, res) => {
   const user = users[req.session.userId] || '';
   const templateVars = {user, urls: urlsForUser(user.id)};
   res.render('urls_index', templateVars);
 });
 
 // Give (GET) the user a form to add new URLs, if not authenticated, return to login page
-app.get("/urls/new", (req, res) => {
+app.get('/urls/new', (req, res) => {
   const user = users[req.session.userId] || '';
   if (!user) {
-    res.redirect("/login");
+    res.redirect('/login');
   }
   const templateVars = {user};
-  res.render("urls_new", templateVars);
+  res.render('urls_new', templateVars);
 });
 
 // Adds (Post) URLs to the user profile
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
   const user = users[req.session.userId] || '';
 
   if (user) {
@@ -160,46 +164,48 @@ app.post("/urls", (req, res) => {
     urlDatabase[randomKey] = {longURL: req.body.longURL, userID: user.id};
   }
   const templateVars = {user,  urls: urlsForUser(user.id)};
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
 
 // Allows user to delete own URLs
-app.post("/urls/:shortURL/delete", (req, res) => {
+app.post('/urls/:shortURL/delete', (req, res) => {
   const user = users[req.session.userId] || '';
   if (user.id === urlDatabase[req.params.shortURL].userID) {
     delete urlDatabase[req.params.shortURL];
   }
   const templateVars = {user,  urls: urlsForUser(user.id) };
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
 
 // Display (GET) the informations for a short url
-app.post("/urls/:shortURL/update", (req, res) => {
+app.post('/urls/:shortURL/update', (req, res) => {
   const user = users[req.session.userId] || '';
   const templateVars = {user, shortURL: req.params.shortURL, url: urlDatabase[req.params.shortURL], toUpdate : true};
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
 // Display (GET) the informations for a short url
-app.get("/urls/:shortURL", (req, res) => {
+app.get('/urls/:shortURL', (req, res) => {
   const user = users[req.session.userId] || '';
   const templateVars = {user, shortURL : req.params.shortURL, url: urlDatabase[req.params.shortURL], toUpdate : false};
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
 // Process (POST) the form for modifying a URL
-app.post("/urls/:shortURL", (req, res) => {
+app.post('/urls/:shortURL', (req, res) => {
   const user = users[req.session.userId] || '';
   if (user.id === urlDatabase[req.params.shortURL].userID) {
     urlDatabase[req.params.shortURL].longURL = req.body.longURL;
   }
   const templateVars = {user, urls: urlsForUser(user.id)};
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
 
 // If nothing is found, default of 404
 app.use((req, res) => {
-  res.render('404');
+  const user = users[req.session.userId] || '';
+  const templateVars = {user};
+  res.render('404',templateVars);
 });
 
 app.listen(PORT, () => {
--- test/helpersTest.js
+++ test/helpersTest.js
@@ -2,22 +2,22 @@ const { assert } = require('chai');
 const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+  'userRandomID': {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: 'purple-monkey-dinosaur'
   },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
+  'user2RandomID': {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk'
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers);
-    const expectedOutput = "userRandomID";
+    const user = getUserByEmail('user@example.com', testUsers);
+    const expectedOutput = 'userRandomID';
     assert.strictEqual(user,expectedOutput);
   });
 });
